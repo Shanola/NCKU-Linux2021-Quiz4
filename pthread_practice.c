05e8d60924b0fe03edd734b4bdf6abfc6a4c6265
David <shanolaola@gmail.com>
2021-03-20 13:51:49 +0800

    Pthread without mutex

--- pthread_practice.c
+++ pthread_practice.c
@@ -2,24 +2,25 @@
 #include <pthread.h>
 #include <unistd.h>
 
-void *child(void *data)
+int cnt = 0;
+void *child()
 {
-    char *str = (char *)data;
-	for (int i=0; i<3; i++) {
-	    printf("%s\n", str);
+	for (int i=0; i<1000000; i++) {
+	    /*int tmp = cnt;
 		sleep(1);
+		cnt = tmp + 1;*/
+		cnt = cnt + 1;
+		printf("cnt = %d\n", cnt);
 	}
 	pthread_exit(NULL);
 }
-
+/* Expexcted 2*1000000 but not really */
 int main(void)
 {
-    pthread_t t;
-	pthread_create(&t, NULL, child, "Child");
-	for (int i=0; i<3; i++) {
-	    printf("Main\n");
-		sleep(1);
-	}
-    pthread_join(t, NULL);
+    pthread_t t1, t2;
+	pthread_create(&t1, NULL, child, "NULL");
+	pthread_create(&t2, NULL, child, "NULL");
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
 	return 0;
 }
