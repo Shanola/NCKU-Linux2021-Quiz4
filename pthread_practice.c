1965188edd9b68f52a308e6a306ee2db1627bc4d
David <shanolaola@gmail.com>
2021-03-20 14:43:53 +0800

    Pthread try with mutex and semaphore

--- pthread_practice.c
+++ pthread_practice.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <time.h>
 
 double tvgetf()
@@ -17,30 +18,36 @@ double tvgetf()
 }
 
 int cnt = 0;
+sem_t semaphore;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *child()
 {
-	for (int i=0; i<1000000; i++) {
-	    /*int tmp = cnt;
-		sleep(1);
-		cnt = tmp + 1;*/
+	for (int i=0; i<3; i++) {
+	    sem_wait(&semaphore);
 		pthread_mutex_lock(&mutex);
-		cnt = cnt + 1;
+		int tmp = cnt;
+		cnt = tmp + 1;
 		pthread_mutex_unlock(&mutex);
-		//printf("cnt = %d\n", cnt);
+		//cnt = cnt + 1;
+		printf("cnt = %d\n", cnt);
 	}
 	pthread_exit(NULL);
 }
 int main(void)
 {
-	double t11 = tvgetf();
-    pthread_t t1, t2;
+	// double t11 = tvgetf();
+    sem_init(&semaphore, 0, 0);
+	pthread_t t1, t2;
 	pthread_create(&t1, NULL, child, "NULL");
 	pthread_create(&t2, NULL, child, "NULL");
+
+	for(int i=0; i<6; i++) {
+	    sem_post(&semaphore);
+	}
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
-	double t22 = tvgetf();
-	printf("%.6f\n", (t22 - t11) * 1000);
+	// double t22 = tvgetf();
+	// printf("%.6f\n", (t22 - t11) * 1000);
 	return 0;
 }
